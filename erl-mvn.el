451a8b81d650166476794c63b298c89eeedabe6a
Sven Heyll <sven.heyll@gmail.com>
2011-02-24 23:02:21 +0100

    work in progress

--- erl-mvn.el
+++ erl-mvn.el
@@ -37,7 +37,7 @@
 Starts a single erl node and invokes maven in each project to compile upload module to the erlang node.\
 This enables distel to work. The emacs distel environment will automatically be connected to that test node, if not already done."
   (interactive "DWorkspace directory: ")
-  (erl-mvn-start-node erl-mvn-node-name)
+  (erl-mvn-start-node erl-mvn-node-name)  
   (message "Searching for poms in: %s" workspace-dir)
   (let ((ws-sub-dirs (directory-files workspace-dir 'full-name "^[^.].+")))
     (delete-if-not (function file-directory-p) ws-sub-dirs)
@@ -48,7 +48,7 @@ This enables distel to work. The emacs distel environment will automatically be 
 (defun erl-mvn-build-project(proj-dir)
   "If the pom file exists, invokes maven to compile and upload the code into the test node"
   (interactive "DProject directory: ")
-  (let ((pom-file (concat proj-dir "/pom.xml")))
+  (let ((pom-file (file-truename (concat proj-dir "/pom.xml"))))
     (if (erl-mvn-is-maven-erlang-project pom-file)
         (erl-mvn-compile-project-maven pom-file)
       (message "No maven-erlang project found in %s" proj-dir))))
@@ -60,12 +60,7 @@ This enables distel to work. The emacs distel environment will automatically be 
         (include-dirs '())
         (code-paths '()))
     (message "Building project %s of type %s" artifact-id packaging)
-    (let ((mvn-result
-           (process-lines erl-mvn-maven-executable
-                          "-f" pom-file
-                          "-DshutdownTestNode=false"
-                          (concat "-DtestNode=" (erl-mvn-complete-node erl-mvn-node-name))
-                          "erlang:show-build-info")))
+    (let ((mvn-result (erl-mvn-run-maven pom-file)))
       (mapcar 
        (lambda(l) 
          (cond ((string-match "include_dir: \\(.+\\)$" l)
@@ -76,6 +71,29 @@ This enables distel to work. The emacs distel environment will automatically be 
       (set (erl-mvn-make-include-path-symbol artifact-id) include-dirs)
       (set (erl-mvn-make-code-path-symbol artifact-id) code-paths))))
 
+(defun erl-mvn-run-maven(pom)
+  "Private function. Runs maven and pastes the output into a buffer, so the user is able to follow the output."
+    (save-excursion
+      (with-current-buffer (get-buffer-create "*maven-output*")
+        (setq buffer-read-only nil)
+        (save-selected-window          
+          (select-window (or (get-buffer-window (current-buffer))
+                             (display-buffer (current-buffer))))
+          (goto-char (point-max))
+          (let ((start (point)))
+            (call-process erl-mvn-maven-executable
+                          nil ; infile
+                          "*maven-output*"   ; current buffer
+                          't   ; redisplay
+                          "-f" pom
+                          "-DshutdownTestNode=false"
+                          (concat "-DtestNode=" 
+                                  (erl-mvn-complete-node erl-mvn-node-name))
+                          "erlang:show-build-info"
+                          "test-compile")
+            (setq buffer-read-only 't)
+            (erl-mvn-to-lines (buffer-substring-no-properties start (point-max))))))))
+
 (defun erl-mvn-is-maven-erlang-project(pom-file)
   "Private function. Returns t if the pom-file exists and defines an erlang project."  
   (if (file-exists-p pom-file) 
@@ -84,12 +102,6 @@ This enables distel to work. The emacs distel environment will automatically be 
                  (string= packaging allowed))
                erl-mvn-erlang-mvn-packaging-types))
     'nil))
-
-(defun erl-mvn-pom-lookup(pom tag)
-  "Private function. Get a child of a the project element from the pom"
-   (let* ((root (car (xml-parse-file pom)))
-         (res-node (xml-get-children (xml-node-children root) tag)))
-     (car (xml-node-children (car res-node)))))
 
 (defun erl-mvn-start-node(node-name)
   "Private function. Starts an erlang node for the node-name that can be used by maven for tests and debug code"
@@ -117,28 +129,56 @@ This enables distel to work. The emacs distel environment will automatically be 
 
 (defun erl-mvn-distel-connect-node (node-name) 
   "Private function."     
-  (let (n (erl-mvn-complete-node-distel node-name))
+  (let ((n (erl-mvn-complete-node-distel node-name)))
     (setq erl-nodename-cache n)
     (erl-ping n)))
 
 ;; ----------------------------------------------------------------------
-;; These functions allow fast compilation and test execution.
+;; These functions relate to buffers or buffer contentes and allow fast
+;; compilation and test execution
 ;; ----------------------------------------------------------------------
 
 (defun erl-mvn-compile-buffer ()
-  "Private function. Compile the file of the buffer with the corresponding erlang node."
+  "Compile the file of the buffer with the corresponding erlang node."
   (interactive)
-  (let ((fn (file-truename (buffer-file-name)))
-	(node (erl-mvn-complete-node-distel (erl-mvn-node-name-for-buffer))))
-    (message (symbol-name node))
-    (erpc node 'c 'c `(,fn))))
+  (let* ((fn (file-truename (buffer-file-name)))
+         (pom-file (erl-mvn-find-pom fn))
+	 (artifact-id (erl-mvn-pom-lookup pom-file 'artifactId))
+         (node (erl-mvn-complete-node-distel erl-mvn-node-name)))
+    (message "Compiling %s from project %s" fn artifact-id)
+    (with-current-buffer "*erl-output*"
+      (kill-region (point-min) (point-max)))
+    (erpc node 'code 'add_paths 
+          (list (eval (erl-mvn-make-code-path-symbol artifact-id))))
+    (erl-rpc 
+     (lambda (result) 
+       (mcase result
 
-(defun erl-mvn-get-erlang-compile-options (file-name)
-  "Private function. Returns a list of compiler arguments for compiling a source file of a\
-project identified by a file of that project."
-  (let* ((pom-file (erl-mvn-find-pom file-name))
-	 (
+         (['ok module] 
+          (message "Successfully compiled module: %s." module)
+         (with-current-buffer "*erl-output*"
+           (fundamental-mode)))
 
+         (['error reasons] 
+          (message "Compilation failed!")
+          (with-current-buffer "*erl-output*"            
+            (compilation-mode))
+          (erl-mvn-process-compilation-error reasons))
+         
+         (unexpected
+          (message "Unexpected message %s" unexpected))))
+     'nil
+     node 
+     'c 'c (cons fn (list (erl-mvn-get-erlang-compile-options artifact-id))))))
+
+(defun erl-mvn-test-buffer()
+  "Compile the buffer and runs eunit test with the module contained in the buffer"
+  'todo)
+
+(defun erl-mvn-test-function-under-point()
+  "Compile the buffer and runs eunit test with the module contained in the buffer"
+  'todo)
+  
 ;; ----------------------------------------------------------------------
 ;; Common utility functions
 ;; ----------------------------------------------------------------------
@@ -166,3 +206,34 @@ for the project identified by an artifact id."
   "Private function. Returns a symbol for the variable that contains the include path list\
 for the project identified by an artifact id."
   (intern (concat "erl-mvn-project-" artifact-id "-include-dirs")))
+
+(defun erl-mvn-pom-lookup(pom tag)
+  "Private function. Get a child of a the project element from the pom"
+   (let* ((root (car (xml-parse-file pom)))
+         (res-node (xml-get-children (xml-node-children root) tag)))
+     (car (xml-node-children (car res-node)))))
+
+(defun erl-mvn-to-lines(str)
+  "Private function. Convert a string with newline characters into a list where each element contains one line. The newline character will be discarded"
+  (let ((current-line '())
+        (lines '()))
+    (mapcar 
+     (lambda (c) 
+       (if (eq c ?\n)
+           (progn (setq lines (cons (eval `(string ,@(reverse current-line))) lines))
+                  (setq current-line '()))  
+         (setq current-line (cons c current-line))))
+     str)
+    ; append the rest
+    (setq lines (cons (eval `(string ,@(reverse current-line))) lines))
+    (reverse lines)))
+
+(defun erl-mvn-get-erlang-compile-options (artifact-id)
+  "Private function. Returns a list of compiler arguments for compiling a source file of a project identified by a maven project artifact-id."
+  (append '(debug_info return report verbose export_all)
+          (mapcar (lambda(dir) (tuple 'i dir))
+                  (eval (erl-mvn-make-include-path-symbol artifact-id)))))
+
+;; ----------------------------------------------------------------------
+;;  Tests
+;; ----------------------------------------------------------------------
